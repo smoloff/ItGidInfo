b834976a46e90a7fed2844110d9c7b7df17a538c
smoloff <smoloff@gmail.com>
2022-03-12 18:21:46 +0200

    Local storage: setItem and getItem, JSON.stringify, JSON.parse

--- unit_23/unit_23.js
+++ unit_23/unit_23.js
@@ -1,49 +1,69 @@
-
+function toOut(a, b) {
+    document.querySelector(`.out-${a}`).innerHTML = b;
+}
 // Task 1 ============================================
 /* Создайте функцию t1 которая записывает  в LS  ключ 5 со значением 11. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-1. */
 
 function t1() {
+    localStorage.setItem(5, 11);
 }
-
-// ваше событие здесь!!!
+let b1 = document.querySelector('.b-1');
+b1.onclick = t1;
 
 // Task 2 ============================================
 /* Создайте функцию t2 которая записывает  в LS  массив a2 = [7,6,5]. Ключ a2. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-2. */
 
-function t2() {
+let a2 = [7, 6, 5];
 
+function t2() {
+    localStorage.setItem('a2', JSON.stringify(a2))
 }
 
-// ваше событие здесь!!!
+let b2 = document.querySelector('.b-2');
+b2.onclick = t2;
 
 
 // Task 3 ============================================
 /*  При нажатии кнопки t3 выведите из LS сохранненный массив a2. Выведите в out-3 в формате ключ пробел значение перенос строки.  */
 
 function t3() {
+    let y = localStorage.getItem('a2');
+    y = JSON.parse(y);
+    y = y.join(' <br>');
 
+    toOut(3, y);
 }
 
-// ваше событие здесь!!!
+let b3 = document.querySelector('.b-3');
+b3.onclick = t3;
 
 
 // Task 4 ============================================
 /*  Создайте функцию t4 которая записывает  в LS  массив a4 = {hello: world, hi:mahai}. Ключ a4. Проверьте что происходит при повторном вызове функции. Запускается ф-я по кнопкуе b-4.*/
 
-function t4() {
+let a4 = { hello: 'world', hi: 'mahai' };
 
+function t4() {
+    localStorage.setItem('a4', JSON.stringify(a4));
 }
 
-// ваше событие здесь!!!
+let b4 = document.querySelector('.b-4');
+b4.onclick = t4;
 
 // Task 5 ============================================
 /*   При нажатии кнопки b-5 выведите из LS сохранненный массив a4. Выведите в out-5 в формате ключ пробел значение перенос строки. */
 
 function t5() {
+    let z = localStorage.getItem('a4');
+    z = JSON.parse(z);
+    z = Object.values(z);
+    z = z.join(' <br>');
 
+    toOut(5, z);
 }
 
-// ваше событие здесь!!!
+let b5 = document.querySelector('.b-5');
+b5.onclick = t5;
 
 // Task 6 ============================================
 /*  Создайте функцию t6 которая очищает весь LS. Запуск по кнопке b-6*/
